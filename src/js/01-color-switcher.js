28cbdc7c558922f551ed567c66e393f129bd9994
Karina Doskuch <karina.doskuch@gmail.com>
2022-11-04 16:49:10 +0200

    Task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function changeBodyBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+class ColorSwitcher {
+  constructor(changeBackgroundColor, startBtnSelector, stopBtnSelector) {
+    this.intervalID = undefined;
+    this.changeBackgroundColor = changeBackgroundColor;
+    this.startBtnRef = document.querySelector(startBtnSelector);
+    this.stopBtnRef = document.querySelector(stopBtnSelector);
+    this.stopBtnRef.disabled = true;
+
+    this.startBtnRef.addEventListener('click', () => this.startChangeColor());
+    this.stopBtnRef.addEventListener('click', () => this.stopChangeColor());
+  }
+
+  startChangeColor() {
+    if (this.startBtnRef.disabled) {
+      return;
+    }
+
+    this.startBtnRef.disabled = true;
+    this.stopBtnRef.disabled = false;
+
+    this.intervalID = setInterval(
+      () => this.changeBackgroundColor(getRandomHexColor()),
+      1000
+    );
+  }
+
+  stopChangeColor() {
+    this.startBtnRef.disabled = false;
+    this.stopBtnRef.disabled = true;
+
+    clearInterval(this.intervalID);
+  }
+}
+
+const colorSwitcher = new ColorSwitcher(
+  changeBodyBackgroundColor,
+  'button[data-start]',
+  'button[data-stop]'
+);
