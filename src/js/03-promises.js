483e7a7028071b4682b22abea670935e1f28b6f1
Karina Doskuch <karina.doskuch@gmail.com>
2022-11-05 15:55:51 +0200

    Task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promiseObject = { position, delay };
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(promiseObject);
+      } else {
+        reject(promiseObject);
+      }
+    }, delay);
+  });
+}
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const currentTarget = event.currentTarget;
+  let delay = parseInt(currentTarget.delay.value);
+  const amount = parseInt(currentTarget.amount.value);
+  const step = parseInt(currentTarget.step.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
